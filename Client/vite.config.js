f731f1933002bb58f30b2dd63ddc5a43ea9cbfb5
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2025-04-08 22:58:26 +0300

    updates

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   server: {
     port: 5173,
     proxy: {
@@ -11,6 +13,11 @@ export default defineConfig({
         target: 'http://localhost:3001',
         ws: true
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 }) 
